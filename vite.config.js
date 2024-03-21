69c0ffad577d78903c9da59863441691a1e0e203
AtulaRiver <ngiang1309@gmail.com>
2024-03-21 22:12:55 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, "index.html"),
                 shop: resolve(__dirname, "./pages/shop.html"),
+                detail: resolve(__dirname, "./pages/detail.html"),
+                pay: resolve(__dirname, "./pages/pay.html"),
+                cart: resolve(__dirname, "./pages/cart.html"),
             },
         },
     },
