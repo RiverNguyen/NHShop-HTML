ca955411f98fd1ccbfcc5595b019b3891290ad11
RiverNguyen <ngiang1309@gmail.com>
2024-03-23 20:50:49 +0700

    upload

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
+                home: resolve(__dirname, "./pages/home.html"),
                 shop: resolve(__dirname, "./pages/shop.html"),
                 detail: resolve(__dirname, "./pages/detail.html"),
                 cart: resolve(__dirname, "./pages/cart.html"),
